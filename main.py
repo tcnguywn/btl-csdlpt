fa60beb9b84f5f4184ad0282ef73dc6373829210
Truong Nguyen <nguyentruong260804@gmail.com>
2025-06-10 23:25:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,26 +5,26 @@ FILE_PATH = r"D:\Giao trinh\csdl phan tan\bai_tap_lon_CSDL_phan_tan\test_data.da
 conn = getopenconnection("postgres", "root", "db_assign1")
 print("get connection success")
 
-# loadratings('ratings', FILE_PATH, conn)
-# print("load successful")
+loadratings('ratings', FILE_PATH, conn)
 
-# rangepartition('ratings', 5, conn)
-# print("new partitions created")
 
-# # Thêm các dòng này để test round robin
+rangepartition('ratings', 5, conn)
+
+roundrobinpartition('ratings', 5, conn)
+
+
+
 # print("\n=== Testing Round Robin Functions ===")
 
 # Tạo 5 partitions
 # roundrobinpartition('ratings', 5, conn)
 # print("new partitions created")
 # # Test insert
-roundrobininsert('ratings', 9999, 1001, 4.5, conn)
-print("new record inserted")
+# roundrobininsert('ratings', 9999, 1001, 4.5, conn)
+# print("new record inserted")
 #
 # rangeinsert('ratings', 145, 8, 4, conn)
 # print("insert succesfully")
-
-
 
 # Đóng connection
 conn.close()
